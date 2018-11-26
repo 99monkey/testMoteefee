bfb37d6772af162f4178debb7fc8060945cc6d8c
Eugene Varlamov <varlamoved@gmail.com>
2018-11-26 09:13:17 +0300

    first way

--- src/App.js
+++ src/App.js
@@ -2,27 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  state = {progressStep: 1}
+  progressStates = ['Design', 'Build', 'Launch']
+
+  get needShowPrev() {
+    return this.state.progressStep > 0
+  }
+
+  onClickNext = () => {
+    this.setState({progressStep: this.state.progressStep + 1})
+
+  }
+  onClickPrev= () => {
+    this.setState({progressStep: this.state.progressStep - 1})
+  }
+
   render() {
+    const {progressStep} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Progress states={this.progressStates} active={progressStep} />
+        {this.needShowPrev && <button onClick={this.onClickPrev}>prev</button> }
+        <button onClick={this.onClickNext}>next</button>
       </div>
     );
   }
+}
+
+const Progress = ({states, active}) => {
+  //state = {active: 0}
+  const points= states.map((s, i) => <ProgressEntry name={s} active={i <= active } />)
+  return (
+    <div class="b-progress">
+      { points }
+    </div>
+  )
+}
+
+
+const ProgressEntry= ({name, active = false }) => {
+  const className = active ? "__active" : ""
+  return (
+    <div class={`progress-entry ${className}`}>
+      <div class="progress-connector"/>
+      <div class="progress-el">
+        <span class="progress-text">{name}</span>
+        <div class="progress-circle"/>
+      </div>
+    </div>
+  )
 }
 
 export default App;
